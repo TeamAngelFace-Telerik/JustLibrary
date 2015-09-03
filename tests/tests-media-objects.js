54daa5e730af327a0200ae00f77dde44c054c94e
damy90 <dany90@gmail.com>
2015-09-04 02:15:48 +0300

    Fix broken merge
    Revert "big-item-template LINK and TRAILER are active links"
    
    This reverts commit ad3c052473855cdd568234274b0f8565f490301f.

--- tests/tests-media-objects.js
+++ tests/tests-media-objects.js
@@ -1,5 +1,6 @@
 /* globals describe, it, require */
-var expect = require('chai').expect;
+import chai from '../node_modules/chai/chai';
+var expect = chai.expect;
 import media from '../app/media/media';
 import book from '../app/media/book';
 import song from '../app/media/song';
@@ -25,10 +26,21 @@ describe('Creation of media', function () {
 
             expect(fn).not.to.throw();
         });
+        it('Expect media to be created correctly', function () {
+            media.init(testMedia);
+
+            expect(media.url).to.equal(testMedia.url);
+            expect(media.image).to.equal(testMedia.image);
+            expect(media.title).to.equal(testMedia.title);
+            expect(media.description).to.equal(testMedia.description);
+            expect(media.rating).to.equal(testMedia.rating);
+            expect(media.genre).to.equal(testMedia.genre);
+        });
     });
     describe('Invalid creation', function () {
         it('Media should throw with invalid title', function () {
             testMedia.title = "Ye";
+
             function fn() {
                 media.init(testMedia);
             }
@@ -37,6 +49,7 @@ describe('Creation of media', function () {
         });
         it('Media should throw with invalid description', function () {
             testMedia.description = "Yey be";
+
             function fn() {
                 media.init(testMedia);
             }
@@ -45,6 +58,7 @@ describe('Creation of media', function () {
         });
         it('Media should throw with invalid rating - string', function () {
             testMedia.rating = "Ye";
+
             function fn() {
                 media.init(testMedia);
             }
@@ -53,6 +67,7 @@ describe('Creation of media', function () {
         });
         it('Media should throw with invalid rating - number', function () {
             testMedia.rating = "138";
+
             function fn() {
                 media.init(testMedia);
             }
@@ -61,6 +76,7 @@ describe('Creation of media', function () {
         });
         it('Media should throw with invalid genre', function () {
             testMedia.genre = "Ye";
+
             function fn() {
                 media.init(testMedia);
             }
@@ -86,15 +102,21 @@ describe('Creation of book', function () {
     describe('Valid creation', function () {
         it('Book to be successfuly created with correct params', function () {
             function fn() {
-                book.init(testBook)
+                book.init(testBook);
             }
             expect(fn).to.not.throw(Error);
+        });
+        it('Expect book to be correctly initialised', function () {
+            book.init(testBook);
+            expect(book.author).to.equal(testBook.author);
+            expect(book.publisher).to.equal(testBook.publisher);
         });
     });
 
     describe('Invalid creation', function () {
         it('Book should throw with invalid publisher', function () {
             testBook.publisher = "38";
+
             function fn() {
                 book.init(testBook);
             }
@@ -103,13 +125,14 @@ describe('Creation of book', function () {
         });
         it('Book should throw with invalid author', function () {
             testBook.author = "Ye";
+
             function fn() {
                 book.init(testBook);
             }
 
             expect(fn).to.throw();
         });
-    })
+    });
 });
 describe('Creation of song', function () {
     var testSong = {};
@@ -132,10 +155,15 @@ describe('Creation of song', function () {
 
             expect(fn).not.to.throw();
         });
+        it('Expect song to be correctly initialised', function () {
+            song.init(testSong);
+            expect(song.duration).to.equal(testSong.duration);
+        });
     });
     describe('Invalid creation', function () {
         it('Song should throw with invalid duration', function () {
             testSong.duration = "";
+
             function fn() {
                 song.init(testSong);
             }
@@ -166,10 +194,16 @@ describe('Creation of video', function () {
 
             expect(fn).not.to.throw();
         });
+        it('Expect video to be correctly initialised', function () {
+            video.init(testVideo);
+            expect(video.duration).to.equal(testVideo.duration);
+            expect(video.trailer).to.equal(testVideo.trailer);
+        });
     });
     describe('Invalid creation', function () {
         it('Video should throw with invalid duration', function () {
             testVideo.duration = "";
+
             function fn() {
                 video.init(testVideo);
             }
@@ -178,11 +212,12 @@ describe('Creation of video', function () {
         });
         it('Video should throw with invalid trailer', function () {
             testVideo.trailer = "";
+
             function fn() {
                 video.init(testVideo);
             }
 
             expect(fn).to.throw();
         });
-    })
+    });
 });
