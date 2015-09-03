1227ef966c68eddb3c8c0d9c5e939b631df10703
damy90 <dany90@gmail.com>
2015-09-03 15:32:42 +0300

    Load everlive api from local file

--- app/db-operations.js
+++ app/db-operations.js
@@ -1,52 +1,53 @@
-var db = (function(){
-	var el = new Everlive('dmJhonVhIAcj1iJk');
+import Everlive from "../lib/everlive.all.min";
+var db = (function () {
+    var el = new Everlive('dmJhonVhIAcj1iJk');
 
-	var db = {
-		create: function(dataType, dataObj, successCb, errorCb){
-			var data = el.data(dataType);
-			return data.create(dataObj).then(successCb, errorCb);
-		},
-		read: function(dataType, currentPage){
-			var data = el.data(dataType),
-				query = new Everlive.Query(),
-				pageSize = 3,
-				page = currentPage || 1;
+    var db = {
+        create: function (dataType, dataObj, successCb, errorCb) {
+            var data = el.data(dataType);
+            return data.create(dataObj).then(successCb, errorCb);
+        },
+        read: function (dataType, currentPage) {
+            var data = el.data(dataType),
+                query = new Everlive.Query(),
+                pageSize = 3,
+                page = currentPage || 1;
 
 
-			query.skip((page-1)*pageSize).take(pageSize);
-			return data.get(query)
-		    .then(function(data){
-		        return data.result;
-		    },
-		    function(error){
-		        return error;
-		    });
-		},
-		readById: function(dataType, id){
-			var data = el.data(dataType);
+            query.skip((page - 1) * pageSize).take(pageSize);
+            return data.get(query)
+                .then(function (data) {
+                        return data.result;
+                    },
+                    function (error) {
+                        return error;
+                    });
+        },
+        readById: function (dataType, id) {
+            var data = el.data(dataType);
 
-			return data.getById(id)
-			    .then(function(data){
-			        return data.result;
-			    },
-			    function(error){
-			        return error;
-			    });
-		},
-		getItemsCount: function(dataType){
-			var data = el.data(dataType);
-			return data.count()
-			    .then(function(data){
-			        return data.result;
-			    },
-			    function(error){
-			        alert(JSON.stringify(error));
-			    });
-		}
-	};
+            return data.getById(id)
+                .then(function (data) {
+                        return data.result;
+                    },
+                    function (error) {
+                        return error;
+                    });
+        },
+        getItemsCount: function (dataType) {
+            var data = el.data(dataType);
+            return data.count()
+                .then(function (data) {
+                        return data.result;
+                    },
+                    function (error) {
+                        alert(JSON.stringify(error));
+                    });
+        }
+    };
 
-	return db;
-	
+    return db;
+
 }());
 
 export default db;
